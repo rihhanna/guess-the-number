521635c345eafc387fcd4f7edb30b0b8ee319319
Abdulladif Roble <45190868+ARoble@users.noreply.github.com>
2022-11-26 09:25:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 //code
 // gets a random number from 1 - 20
-const randomNumber = Math.floor(Math.random() * 20) + 1
+let randomNumber = Math.floor(Math.random() * 20) + 1
+let score = 20
+let highScore = 0
